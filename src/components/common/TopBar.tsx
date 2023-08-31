aaf179c47c83a500197076e3beba22a5d985304d
Haeun Kim <52477327+ckhe1215@users.noreply.github.com>
2023-08-31 23:54:46 +0900

    :sparkles: Top Bar 컴포넌트 추가 (#2)

---
+++ src/components/common/TopBar.tsx
@@ -0,0 +1,53 @@
+import colors from "@/styles/colors";
+import HeartIcon from "@/svg/HeartIcon";
+import Logo from "@/svg/Logo";
+import PersonIcon from "@/svg/PersonIcon";
+import { FC } from "react";
+import { styled } from "styled-components";
+
+const TopBar: FC = () => {
+  return (
+    <Header>
+      <Logo />
+      <ButtonGroup>
+        <IconButton>
+          <HeartIcon />
+        </IconButton>
+        <IconButton>
+          <PersonIcon />
+        </IconButton>
+      </ButtonGroup>
+    </Header>
+  );
+};
+
+export default TopBar;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-inline: 16px;
+  border-bottom: 1px solid ${colors.GRAY[1]};
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  align-items: center;
+  padding-block: 16px;
+
+  button:first-child {
+    margin-right: 12px;
+  }
+`;
+const IconButton = styled.button`
+  background-color: transparent;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+`;

6030d5a2099d8ab87d837183be240d9449b3c580
KyeongS <59828677+leeks9653@users.noreply.github.com>
2023-09-17 02:43:39 +0900

    :zap: tap 메뉴 완료 , 버튼 컴포넌트 속성 에러 수정

---
+++ src/components/common/TapMenu.tsx
@@ -0,0 +1,65 @@
+import { FC } from "react";
+import colors from "../../styles/colors";
+import { getFonts } from "../../styles/fonts";
+import { styled } from "styled-components";
+
+interface TapMenuOption {
+  label: string;
+  value: any;
+}
+
+interface TapMenuProps {
+  /**
+   * 현재 값
+   */
+  value: any;
+  /**
+   * 버튼목록에 들어갈 값 배열
+   */
+  options: TapMenuOption[];
+  /**
+   * 값 변경 콜백
+   */
+  onChange: (value: any) => void;
+}
+
+const TapMenu: FC<TapMenuProps> = (props) => {
+  const { value, onChange, options } = props;
+  return (
+    <TabBox>
+      {options.map((option) => {
+        return (
+          <TabButton
+            key={option.value}
+            active={value === option.value}
+            onClick={() => {
+              onChange(option.value);
+            }}
+          >
+            {option.label}
+          </TabButton>
+        );
+      })}
+    </TabBox>
+  );
+};
+
+export default TapMenu;
+
+const TabBox = styled.div`
+  padding-inline: 16px;
+  border-bottom: 1px solid ${colors.GRAY[2]};
+`;
+
+const TabButton = styled.button<{ active: boolean }>`
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+  width: 50%;
+  padding-block: 15px;
+  ${(props) => `
+    ${props.active ? getFonts("H5_SEMIBOLD") : getFonts("H5_REGULAR")}
+    color: ${props.active ? colors.FONT_LIGHT.PRIMARY : colors.FONT_LIGHT.SECONDARY};
+    border-bottom: solid 4px ${props.active ? colors.ON.BASIC_LIGHT : colors.ON.BASIC_DARK};
+  `}
+`;
--- src/stories/ScopeBar.stories.tsx
+++ src/stories/ScopeBar.stories.tsx
@@ -1,27 +1,37 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import ScopeBar from "@/components/common/ScopeBar";
+import TapMenu from "@/components/common/TapMenu";
+import { useState } from "react";
 
 const meta = {
-  title: "Common/ScopeBar",
-  component: ScopeBar,
+  title: "Common/TapMenu",
+  component: () => {
+    const [state, setState] = useState(false);
+
+    const handleChangeState = (value: boolean) => {
+      setState(value);
+    };
+
+    return (
+      <TapMenu
+        onChange={handleChangeState}
+        value={state}
+        options={[
+          {
+            label: "첫번째",
+            value: true,
+          },
+          { label: "두번째", value: false },
+        ]}
+      />
+    );
+  },
   tags: ["autodocs"],
-} satisfies Meta<typeof ScopeBar>;
+} satisfies Meta<typeof TapMenu>;
 
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
-export const ShowAvailableStocks: Story = {
-  args: {
-    isShowingAllStocks: false,
-    availableStocksCount: 8,
-    allStocksCount: 12,
-  },
-};
-
-export const ShowAllStocks: Story = {
-  args: {
-    isShowingAllStocks: true,
-    availableStocksCount: 8,
-    allStocksCount: 12,
-  },
+export const Default: Story = {
+  args: {},
 };

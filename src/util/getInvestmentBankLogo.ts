7180094ab640266ca673730a127e5378eb1a74f3
ckhe1215 <ckhe1215@gmail.com>
2023-09-18 20:04:49 +0900

    :art: 코드 포맷 수정

--- src/util/getInvestmentBankLogo.ts
+++ src/util/getInvestmentBankLogo.ts
@@ -1,10 +1,10 @@
-import nhLogo from "../../public/images/nh_logo.png"
-import hanhwaLogo from "../../public/images/hanhwa_logo.png"
-import hyundaiLogo from "../../public/images/hyundai_logo.png"
-import kbLogo from "../../public/images/kb_logo.png"
-import ibkLogo from "../../public/images/ibk_logo.png"
-import skLogo from "../../public/images/sk_logo.png"
-import yuantaLogo from "../../public/images/yuanta_logo.png"
+import nhLogo from "../../public/images/nh_logo.png";
+import hanhwaLogo from "../../public/images/hanhwa_logo.png";
+import hyundaiLogo from "../../public/images/hyundai_logo.png";
+import kbLogo from "../../public/images/kb_logo.png";
+import ibkLogo from "../../public/images/ibk_logo.png";
+import skLogo from "../../public/images/sk_logo.png";
+import yuantaLogo from "../../public/images/yuanta_logo.png";
 
 // TODO: 증권사 ID 정해지면 name -> id 로 변경
 // TODO: 전체 증권사 이미지 저장하기
@@ -20,6 +20,6 @@ const getInvestmentBankLogo = (name: string) => {
   ]);
 
   return logoMap.get(name) ?? nhLogo;
-}
+};
 
 export default getInvestmentBankLogo;

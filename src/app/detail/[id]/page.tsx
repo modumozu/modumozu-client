72c321b9eb786b168216ffa080b5661630bdfeeb
KyeongS <59828677+leeks9653@users.noreply.github.com>
2023-09-28 16:28:45 +0900

    :sparkles: 상세 수정사항 적용, 컴포넌트 추

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -3,10 +3,33 @@
 import DetailCard from "@/components/detail/DetailCard";
 import DetailHeader from "@/components/detail/DetailHeader";
 import DetailTitle from "@/components/detail/DetailTitle";
-import IPOConfirm from "@/components/detail/IPOConfirm";
+import IPOConfirm, { IPOComfirmTableData } from "@/components/detail/IPOConfirm";
 import IPOInfo from "@/components/detail/IPOInfo";
 import IPOTapMenu from "@/components/detail/IPOTapMenu";
+import InvestCompetition from "@/components/detail/InvestCompetition";
 import POInfo from "@/components/detail/POInfo";
+
+const ipoConfirmDummy: IPOComfirmTableData = [
+  {
+    label: "15일",
+    value: "22,886,000",
+  },
+  {
+    label: "1개월",
+    value: "22,886,000",
+  },
+  {
+    label: "3개월",
+    value: "22,886,000",
+  },
+  {
+    label: "6개월",
+    value: "22,886,000",
+  },
+  { label: "미확약", value: "2,808,389,000" },
+  { label: "합계", value: "3,033,263,000" },
+  { label: "총 수량 대비 비율 (%)", value: "11%" },
+];
 
 const Detail = () => {
   return (
@@ -24,7 +47,10 @@ const Detail = () => {
           <POInfo />
         </DetailCard.item>
         <DetailCard.item>
-          <IPOConfirm />
+          <InvestCompetition />
+        </DetailCard.item>
+        <DetailCard.item>
+          <IPOConfirm data={ipoConfirmDummy} />
         </DetailCard.item>
       </DetailCard.wrapper>
     </div>
--- src/components/detail/POPlan.tsx
+++ src/components/detail/POPlan.tsx
@@ -2,21 +2,73 @@
 
 import colors from "@/styles/colors";
 import { getFonts } from "@/styles/fonts";
+import CheckBackground from "@/svg/CheckBackground";
+import IngIcon from "@/svg/IngIcon";
 import styled from "styled-components";
+import dayjs from "dayjs";
+
+const processes: POPlanProcessList = [
+  { label: "수요 예측일", date: "2023-09-20" },
+  { label: "청약일", date: ["2023-09-23", "2023-10-03"] },
+  { label: "환불일", date: "2023-10-04" },
+  { label: "상장일", date: "2023-10-15" },
+];
+
+export type POPlanProcessList = POPlanProcessObject[];
+interface POPlanProcessObject {
+  label: string;
+  date: string | [string, string];
+}
 
 const POPlan = () => {
+  const renderProcessElement = () => {};
+  const renderProcess = () => {
+    const today = dayjs();
+
+    return processes.map((process, index) => {
+      const { date, label } = process;
+      if (typeof date === "string") {
+        if (index === 0) {
+          return (
+            <POPlanProcessItem key={label}>
+              <CheckBackground />
+              <div>
+                <div>label</div>
+                <div>date</div>
+              </div>
+            </POPlanProcessItem>
+          );
+        }
+        return (
+          <POPlanProcessItem key={label}>
+            <CheckBackground />
+            <div>
+              <div>label</div>
+              <div>date</div>
+            </div>
+          </POPlanProcessItem>
+        );
+      } else {
+        const [start, end] = date;
+        return (
+          <POPlanProcessItem key={label}>
+            <CheckBackground />
+            <div>
+              <div>label</div>
+              <div>date</div>
+            </div>
+          </POPlanProcessItem>
+        );
+      }
+    });
+  };
+
+  renderProcess();
+
   return (
     <POPlanWrap>
       <POPlanTitle>공모 일정</POPlanTitle>
-      <POPlanProcessList>
-        <POPlanProcessItem></POPlanProcessItem>
-        <POPlanProcessItem></POPlanProcessItem>
-        <POPlanProcessItem></POPlanProcessItem>
-        <POPlanProcessItem></POPlanProcessItem>
-        <POPlanProcessItem></POPlanProcessItem>
-        <POPlanProcessItem></POPlanProcessItem>
-        <POPlanProcessItem></POPlanProcessItem>
-      </POPlanProcessList>
+      <POPlanProcessList>{renderProcess()}</POPlanProcessList>
     </POPlanWrap>
   );
 };
@@ -36,4 +88,23 @@ const POPlanProcessList = styled.ul`
   margin-top: 24px;
 `;
 
-const POPlanProcessItem = styled.li``;
+const POPlanProcessItem = styled.li`
+  display: flex;
+  align-items: center;
+`;
+
+const BarWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Bar = styled.div`
+  width: 2px;
+  height: 24px;
+  background-color: ${colors.BLUE[5]};
+`;
+const DisabledBar = styled.div`
+  width: 2px;
+  height: 24px;
+  background-color: ${colors.GRAY[2]};
+`;

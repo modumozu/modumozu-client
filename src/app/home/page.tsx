424352d569b4ce0caf8cff13ee7696b54b9dc6d7
KyeongS <59828677+leeks9653@users.noreply.github.com>
2023-10-21 13:20:24 +0900

    fix: 필요없는 콘솔 제

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import UpcomingStockSection from "@/components/home/UpcomingStockSection";
 import EndedStockSection from "@/components/home/EndedStockSection";
-import { getIPOList } from "@/api/ipo";
 
 const HomePage: FC = () => {
-  useEffect(() => {
-    getIPOList();
-  }, []);
   return (
     <>
       <UpcomingStockSection />
